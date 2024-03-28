7651fa6250a0b4cf3259590805ac005ca90a4cb5
Reza Amiri <reza.amiri.ac@gmail.com>
2024-03-28 11:33:49 +0300

    add redis queue

---
+++ internal/events/integrationtest/main_test.go
@@ -0,0 +1,9 @@
+package integrationtest
+
+import (
+	_ "github.com/ory/dockertest/v3"
+	_ "github.com/redis/rueidis"
+	_ "github.com/stretchr/testify/assert"
+	_ "github.com/stretchr/testify/suite"
+
+)
